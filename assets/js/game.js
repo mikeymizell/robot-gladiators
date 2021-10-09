0e8e42914eb29e8e7fb45ce220d05e48347f9a78
mikeymizell <61428335+mikeymizell@users.noreply.github.com>
2021-10-09 16:00:52 -0700

    updated bug in player name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,8 +71,19 @@ var fight = function(enemy) {
     }
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -124,7 +135,7 @@ var startGame = function() {
 
     for (var i = 0; i <enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
             var pickedEnemyObj = enemyInfo[i];
 
